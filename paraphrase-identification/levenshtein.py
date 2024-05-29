a5c648cd38887215721e739567c480a17baca9d4
Hend Mohammed <110117745+hendmuhmd99@users.noreply.github.com>
2024-05-29 15:40:26 +0000

    zzhihuh

--- paraphrase-identification/levenshtein.py
+++ paraphrase-identification/levenshtein.py
@@ -2,7 +2,7 @@ import nltk
 import pandas as pd
 
 import nltk
-
+# C
 def levenshtein_distance(df):
     distance = df.apply(lambda row: nltk.edit_distance(row["sentence1"], row["sentence2"]), axis=1)
     return distance

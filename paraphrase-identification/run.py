cee4a87e39eb3723e61aacd0c10b06ce1a8789de
Hend Mohammed <110117745+hendmuhmd99@users.noreply.github.com>
2024-05-29 15:48:16 +0000

    vfvdvdvsvsa

--- paraphrase-identification/run.py
+++ paraphrase-identification/run.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-from levenshtein import levenshtein_distance
 from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
+from levenshtein import levenshtein_distance
 
 if __name__ == "__main__":
     # Load the data
@@ -9,6 +9,10 @@ if __name__ == "__main__":
     df = tira.pd.inputs(
         "nlpbuw-fsu-sose-24", "paraphrase-identification-validation-20240515-training"
     ).set_index("id")
+
+    # Ensure 'sentence1' and 'sentence2' columns are present
+    if 'sentence1' not in df.columns or 'sentence2' not in df.columns:
+        raise ValueError("DataFrame does not contain 'sentence1' and 'sentence2' columns.")
 
     # Compute the Levenshtein distance
     df["distance"] = levenshtein_distance(df)

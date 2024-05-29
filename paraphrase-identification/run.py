ba87972634b18cf26b8f8f10a641fd50691933e5
Hend Mohammed <110117745+hendmuhmd99@users.noreply.github.com>
2024-05-29 16:06:52 +0000

    fgtdsdawaeefdgfdsasdfgh

--- paraphrase-identification/run.py
+++ paraphrase-identification/run.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from levenshtein import levenshtein_distance
 from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
+import os
 
 if __name__ == "__main__":
     # Load the best threshold from train.py
@@ -19,8 +20,9 @@ if __name__ == "__main__":
     df["label"] = (df["distance"] <= best_threshold).astype(int)
     df = df.drop(columns=["distance", "sentence1", "sentence2"]).reset_index()
 
-    # Save the predictions
-    output_directory = get_output_directory(str(Path(__file__).parent))
-    df.to_json(
-        Path(output_directory) / "predictions.jsonl", orient="records", lines=True
-    )
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    # Combine with the filename to get the absolute path of best_threshold.txt
+    threshold_file = os.path.join(dir_path, 'best_threshold.txt')
+    # Now open the file
+    with open(threshold_file, 'r') as f:
+        best_threshold = float(f.read().strip())

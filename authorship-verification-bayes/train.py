27400f9c6bd7fdc335974324002d9a6243da8a37
Hend Mohammed <110117745+hendmuhmd99@users.noreply.github.com>
2024-05-29 13:26:11 +0000

    mlmmmmmmmmmmmm

--- authorship-verification-bayes/train.py
+++ authorship-verification-bayes/train.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-#from sklearn.externals import joblib
+#from sklearn.externals import joblib,,
 import joblib 
 
 
